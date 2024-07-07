9eae7e9d6b0885e8669058f6842700dac96e40d9
Kiri487 <kiri48787@gmail.com>
2024-07-07 20:41:29 +0800

    [feat] Added logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import "./Header.css";
+import LOGO_WHITE from "../../assets/logo_white.png";
 
 export default function Header() {
   return (
     <div className="header">
-      <div className="header-title">RANsomCheck</div>
+      <div className="header-title">
+        <img src={LOGO_WHITE} />
+        RANsomCheck
+      </div>
       <div className="header-options">
         <a className="header-option" href="/">Home</a>
         <a className="header-option" href="/analysis">Analysis</a>
