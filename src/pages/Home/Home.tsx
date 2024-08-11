4ad214a50bbfcb8beeae60933c03a12aecd34459
Kiri487 <kiri48787@gmail.com>
2024-08-11 21:35:20 +0800

    [feat] Implemented file upload

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,40 @@ import "./Home.css";
 import UPLOAD_FILE_ICON from "../../assets/UploadFile.png";
 
 function FileUpload() {
+  const onFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const formData = new FormData();
+      formData.append("file", file);
+
+      try {
+        const response = await fetch("http://localhost:5000/upload", {
+          method: "POST",
+          body: formData
+        });
+
+        const result = await response.json();
+  
+        if (response.ok) {
+          const userConfirmed = confirm(`${result.message} Do you want to upload another file?`);
+          
+          if (userConfirmed) {
+            (event.target as HTMLInputElement).value = "";
+          } else {
+            window.location.href = "/analysis";
+          }
+        } else {
+          alert(result.error || result.message);
+        }
+      } catch (error) {
+        alert("System error occurred.");
+      }
+    }
+  };
+  
   return (
     <label htmlFor="input" className="file-input">
-      <input hidden id="input" type="file" />
+      <input hidden id="input" type="file" onChange={onFileUpload} />
       <img src={UPLOAD_FILE_ICON} />
       <p>Upload File</p>
     </label>
